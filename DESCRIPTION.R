ef351a77e7c8651c6132b0f7b752b4beed14714e
oliviapikul <oliviapikul@usf.edu>
2025-11-25 12:36:11 -0500

    Create DESCRIPTION.R

---
+++ DESCRIPTION.R
@@ -0,0 +1,10 @@
+Package: Friedman
+Title: Tools for Cleaning Data 
+Version: 0.0.0.9000
+Authors@R: 
+    person("Olivia", "Pikul", , "oliviajpikul@gmail.com", role = c("aut", "cre"))
+Description: Provides collections of tools for cleaning, summarizing, and visualizing data. This package will include functions to help with handling missing values, computing statistics, and generating quality plots using these  functions.
+License: use_mit_license()
+Encoding: UTF-8
+Roxygen: list(markdown = TRUE)
+RoxygenNote: 7.3.3
